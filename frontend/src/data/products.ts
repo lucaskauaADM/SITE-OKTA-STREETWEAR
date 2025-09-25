efc79fcc8f158ab1474a0ebf115620789acbad5e
rods.gamedev.not <144458722+rods-desenvolvedor@users.noreply.github.com>
2025-09-25 13:11:28 -0300

    chore: adicionado manualmente o arquivo com os dados das roupas (será feito CRUD depois

---
+++ frontend/src/data/products.ts
@@ -0,0 +1,31 @@
+import type { Product } from "../types";
+
+export const products: Product[] = [
+  // Camisetas
+  { id: "cam-01", category: "camisetas", image: "/img/camisa01.jpg", name: "Camiseta Street Trip",        price: "R$ 119,90" },
+  { id: "cam-02", category: "camisetas", image: "/img/camisa02.jpg", name: "Camiseta Oversize Trip Side", price: "R$ 119,90" },
+  { id: "cam-03", category: "camisetas", image: "/img/camisa03.jpg", name: "Camiseta Oversize Trip Side", price: "R$ 119,90" },
+  { id: "cam-04", category: "camisetas", image: "/img/camisa04.jpg", name: "Camiseta Oversize Trip Side", price: "R$ 119,90" },
+  { id: "cam-05", category: "camisetas", image: "/img/camisa05.jpg", name: "Camiseta Oversize Trip Side", price: "R$ 119,90" },
+
+  // Bermudas (mantive as imagens conforme seu HTML de exemplo)
+  { id: "ber-01", category: "bermudas", image: "/img/bermuda01.jpg", name: "Bermuda Cargo Trip Side", price: "R$ 149,90" },
+  { id: "ber-02", category: "bermudas", image: "/img/bermuda02.jpg", name: "Bermuda Cargo Trip Side", price: "R$ 149,90" },
+  { id: "ber-03", category: "bermudas", image: "/img/camisa04.jpg",  name: "Bermuda Cargo Trip Side", price: "R$ 149,90" },
+  { id: "ber-04", category: "bermudas", image: "/img/camisa03.jpg",  name: "Bermuda Cargo Trip Side", price: "R$ 149,90" },
+  { id: "ber-05", category: "bermudas", image: "/img/camisa04.jpg",  name: "Bermuda Cargo Trip Side", price: "R$ 149,90" },
+
+  // Calças
+  { id: "cal-01", category: "calcas", image: "/img/calca01.jpg", name: "Calça Street Trip Side", price: "R$ 149,90" },
+  { id: "cal-02", category: "calcas", image: "/img/calca02.jpg", name: "Calça Street Trip Side", price: "R$ 149,90" },
+  { id: "cal-03", category: "calcas", image: "/img/calca03.jpg", name: "Calça Street Trip Side", price: "R$ 149,90" },
+  { id: "cal-04", category: "calcas", image: "/img/calca04.jpg", name: "Calça Street Trip Side", price: "R$ 149,90" },
+  { id: "cal-05", category: "calcas", image: "/img/camisa05.jpg", name: "Calça Street Trip Side", price: "R$ 149,90" },
+
+  // Acessórios
+  { id: "ace-01", category: "acessorios", image: "/img/acessorio01.jpg", name: "Boné Streetwear Trip Side", price: "R$ 139,90" },
+  { id: "ace-02", category: "acessorios", image: "/img/acessorio02.jpg", name: "Boné Streetwear Trip Side", price: "R$ 139,90" },
+  { id: "ace-03", category: "acessorios", image: "/img/acessorio03.jpg", name: "Boné Streetwear Trip Side", price: "R$ 139,90" },
+  { id: "ace-04", category: "acessorios", image: "/img/acessorio04.jpg", name: "JULIET",                   price: "R$ 130,00" },
+  { id: "ace-05", category: "acessorios", image: "/img/acessorio05.jpg", name: "JULIET EYE JACKET",         price: "R$ 130,00" },
+];
